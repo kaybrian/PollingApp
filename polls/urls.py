d5b0165e3cdb013b9f3a33d44c319d5cda345672
kaybrian <kayonbrian@gmail.com>
2020-06-15 23:39:35 +0300

    simple setup is all done for the app

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name='polls'
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('results/',views.result,name='result'),
+    path('vote/',views.vote,name='vote'),
+    path('create',views.Create,name='create'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'polls/index.html')
+    context = {}
+    return render(request,'polls/index.html',context)
+
+def Create(request):
+    context = {}
+    return render(request,'polls/create.html',context)
+
+def vote(request):
+    context = {}
+    return render(request,'polls/vote.html',context)
+
+def result(request):
+    context = {}
+    return render(request,'polls/result.html',context)
